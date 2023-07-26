058020f1a4abe213f42e656aa303b971354fe7f2
mumat0103 <wsx2138@naver.com>
2023-07-26 11:45:51 -0700

    fix:데이터 변경

--- keyword_extract.py
+++ keyword_extract.py
@@ -1,11 +1,19 @@
-import pandas as pd
-from filesystem import FileSystem
-from keybert import KeyBERT
 import os
+import re
+import time
+import pandas as pd
+from keybert import KeyBERT
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
+from filesystem import FileSystem
+
 DIR_PATH = r"C:\Users\wsx21\Desktop\crawling"
 
+
 def find_file():
-    dir_route = f'{DIR_PATH}\\results'
+    dir_route = f'{DIR_PATH}\\reviews'
     fs = FileSystem()
     file_list = fs.get_file_list(dir_route)
     files = []
@@ -15,14 +23,29 @@ def find_file():
 
     return files
 
+
+def data_text_cleaning(data):
+    # 영문자 이외 문자는 공백으로 변환
+    only_english = re.sub('[^a-zA-Z]+', ' ', data)
+    # 불용어 제거
+    stop_words = set(stopwords.words('english'))
+    words = word_tokenize(only_english)
+    no_stops = [word for word in words if word.lower() not in stop_words]
+    # 어간 추출
+    lemmatizer = WordNetLemmatizer()
+    stemmer_words = [lemmatizer.lemmatize(word) for word in no_stops]
+    # 공백으로 구분된 문자열로 결합하여 결과 반환
+    return ' '.join(stemmer_words)
+
+
 def BERT():
     csv_files = find_file()
     for reviews in csv_files:
         print(reviews)
-
+        start_time = time.time()
         index = reviews.find("reviews")
         filename = reviews[index+7:]
-        if os.path.exists(f'{DIR_PATH}\\results\\{filename}'):
+        if os.path.exists(f'{DIR_PATH}\\test\\{filename}'):
             continue
         df = pd.read_csv(reviews)
         bow = []
@@ -30,7 +53,9 @@ def BERT():
         cafe_review = df["1"]
         for review in cafe_review:
             try:
-                keywords = kw_extractor.extract_keywords(review)
+                review = data_text_cleaning(review)
+                keywords = kw_extractor.extract_keywords(
+                    review, keyphrase_ngram_range=(1, 2), stop_words=None)
             except Exception as e:
                 print(e)
                 continue
@@ -42,20 +67,15 @@ def BERT():
                 new_bow.append(bow[i][j])
 
         keyword = pd.DataFrame(new_bow, columns=['keyword', 'weight'])
-        result_df = keyword.groupby('keyword').agg('sum').sort_values('weight', ascending=False)
-        
-        result_df.to_csv(f'{DIR_PATH}\\results\\{filename}')
+        result_df = keyword.groupby('keyword').agg(
+            'sum').sort_values('weight', ascending=False)
 
-def main():
-    csv_files = find_file()
-    keyword = []
-    for result in csv_files:
-        df = pd.read_csv(result)
-        data = df['keyword'].tolist()
-        for value in data:
-            keyword.append(value)
-    keyword = set(keyword)
-    print(sorted(keyword), len(keyword))
+        result_df.to_csv(f'{DIR_PATH}\\test\\{filename}')
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"실행 시간: {execution_time:.6f} 초")
+
 
 if __name__ == "__main__":
-    main()
+    nltk.download('wordnet')
+    BERT()
